7105dedd5ae5669b195189855fac8269c80ecf1e
K-ongpop Nualsaard <kongpopku@ku.th>
2024-10-13 22:34:07 +0700

    push

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const transportsRouter = require('./routes/transports');
+const transportsRouter = require('./routes/tranSport');
 const app = express();
 const port = process.env.PORT || 3000;
 
