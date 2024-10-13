52d7bbec970f17d5444265f027de9726398e9cc7
K-ongpop Nualsaard <kongpopku@ku.th>
2024-10-13 23:32:04 +0700

    dasdasdasd

--- routes/tranSport.js
+++ routes/tranSport.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Transport = require('../models/tranSport'); // Import model
+const Transport = require('../models/tranSport');
 
 // GET - แสดงรายการทั้งหมด
 router.get('/', async (req, res) => {
